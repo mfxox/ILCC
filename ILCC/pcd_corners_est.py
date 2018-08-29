a81ee0c0a2a7e89e63953181dca36b593ece2087
wangwm <wwmsjtu@gmail.com>
2018-08-29 15:07:40 +0900

    (test)  add pcd_est function for vlp32c
    vlp32c for test

--- ILCC/pcd_corners_est.py
+++ ILCC/pcd_corners_est.py
@@ -44,6 +44,10 @@ elif params['LiDAR_type'] == 'hdl64':
 elif params['LiDAR_type'] == 'vlp16_puck':
     h_coef = 2 * np.sin(np.deg2rad(0.25) / 2)
     v_coef = 2 * np.sin(np.deg2rad(2) / 2.)
+elif params['LiDAR_type'] == 'hdl32c':
+    h_coef = 2 * np.sin(np.deg2rad(0.25) / 2)
+    v_coef = 2 * np.sin(np.deg2rad(0.333) / 2.)#non-linear [4.667, -4.   , -3.667, -3.333, -3.   , -2.667, -2.333, -2.   ,-1.333,  0.667,  1.   ,  1.667,  2.333,  3.333,  4.667,  7. , 10.333, 15.]
+    #delte_ang= [0.667,   0.333,   0.334,   0.333,   0.333,   0.334, 0.333,   0.667,   2.   ,   0.333,   0.667,   0.666,   1.   ,1.334,   2.333,   3.333,   4.6673]
 else:
     AssertionError("Please input the right LiDAR_type in the config.yaml")
 
